0dd61a6ade415e727185f6255d2abe7b15625bd2
jihaneh <jijih@hotmail.fr>
2022-04-04 11:32:56 +0200

    Add Class Person

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
+
+julie = Person.new(12, 'julie', parent_permission: false)
+p julie
+p julie.can_use_services?
+
+pierre = Person.new(22, 'Pierre')
+p pierre
+p pierre.can_use_services?
