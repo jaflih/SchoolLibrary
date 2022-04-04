038baac6c341d0a8847d7b8996e5674e821ce3a2
jihaneh <jijih@hotmail.fr>
2022-04-04 15:03:25 +0200

    Add TrimmerDecorator

---
+++ lib/trimmer_decorator.rb
@@ -0,0 +1,15 @@
+require_relative 'base_decorator'
+
+class TrimmerDecorator < BaseDecorator
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    if @nameable.correct_name.length < 10
+      @nameable.correct_name
+    else
+      @nameable.correct_name[0, 10]
+    end
+  end
+end
