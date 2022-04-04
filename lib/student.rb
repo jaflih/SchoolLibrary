8bce870e18652189d45f3b6979d3127d3bc0a2fc
jihaneh <jijih@hotmail.fr>
2022-04-04 11:47:08 +0200

    Add clss Student

---
+++ lib/student.rb
@@ -0,0 +1,16 @@
+require_relative 'person'
+
+class Student < Person
+  def initialize(age, name, parent_permission, classroom)
+    @classroom = classroom
+    super(age, name, parent_permission:)
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
+
+remy = Student.new(24, 'Remy', true, 'classroom')
+p remy
+p remy.play_hooky
