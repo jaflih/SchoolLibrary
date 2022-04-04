26e16bf8bb6ff9e94facac39a72a1c4dd484809b
jihaneh <jijih@hotmail.fr>
2022-04-04 15:03:44 +0200

    Add test

--- app.rb
+++ app.rb
@@ -1,1 +1,10 @@
-# Start the app
+require_relative 'lib/person'
+require_relative 'lib/capitalize_decorator'
+require_relative 'lib/trimmer_decorator'
+
+person = Person.new(22, 12, 'maximilianus')
+p person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+p capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
