9bfc98b560be4d349a18eac4f6402ca44e20bc6c
jihaneh <jijih@hotmail.fr>
2022-04-04 14:34:50 +0200

    Prepare base Decorator

---
+++ lib/base_decorator.rb
@@ -0,0 +1,12 @@
+require_relative 'nameable'
+
+class BaseDecorator < Nameable
+  def initialize(object)
+    @object = object
+    super
+  end
+
+  def correct_name
+    @object.correct_name
+  end
+end
