0796372cfa2e00d61b3ed14095cea170aaac0d40
jihaneh <jijih@hotmail.fr>
2022-04-04 11:53:03 +0200

    Add class teacher

---
+++ lib/teacher.rb
@@ -0,0 +1,16 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(age, name, parent_permission, specialization)
+    @specialization = specialization
+    super(age, name, parent_permission:)
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+timmy = Teacher.new(12, 'Timmy', false, 2)
+p timmy
+p timmy.can_use_services?
