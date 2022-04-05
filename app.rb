d71c742aa266e42485ce97aaabcdc12797a4b41d
jihaneh <jijih@hotmail.fr>
2022-04-05 16:28:03 +0200

    list books and add book

--- app.rb
+++ app.rb
@@ -6,53 +6,16 @@ require_relative 'lib/student'
 require_relative 'lib/book'
 require_relative 'lib/rental'
 
-person = Person.new(22, 12, 'maximilianus')
-p person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-p capitalized_trimmed_person.correct_name
+class App
+  def initialize
+    @books = []
+  end
 
-classroom = Classroom.new('mathematic')
+  def list_all_books
+    @books
+  end
 
-sarrah = Student.new(1, 20, nil, 'Sarrah')
-john = Student.new(2, 20, nil, 'John')
-ines = Student.new(3, 20, nil, 'Ines')
-
-classroom.add_student(sarrah)
-classroom.add_student(ines)
-
-classroom.students.each do |e|
-  print e.name, ' '
-end
-puts
-p 'Sarrah classroom :', sarrah.classroom.label
-
-p 'John classroom :', john.classroom
-
-p 'John classroom :', john.classroom.label unless john.classroom.nil?
-
-john.classroom = classroom
-p 'John classroom :', john.classroom.label
-
-book = Book.new('MyBook', 'Garibaldy')
-book2 = Book.new('MyBook2', 'Garibaldy')
-book3 = Book.new('MyBook3', 'Garibaldy')
-Rental.new('12/12/2000', book, ines)
-Rental.new('12/12/2000', book2, ines)
-Rental.new('12/12/2000', book3, ines)
-
-p 'Ines books : '
-ines.rentals.each do |r|
-  print r.book.title, ' '
-end
-
-p '-------------'
-book.add_rental('mm', john)
-john.rentals.each do |r|
-  print r.book.title, ' '
-end
-
-book.rentals.each do |r|
-  print r.person.name, ' '
+  def create_a_book(title, author)
+    @books.push(Book.new(title, author))
+  end
 end
--- main.rb
+++ main.rb
@@ -1,107 +1,124 @@
-def create_a_student
-  print 'Age : '
-  age = gets.chomp
-  print 'Name : '
-  name = gets.chomp
-  print 'Has parent permission ? [Y/N] '
-  permission = gets.chomp
-  puts 'Person created successfully '
-  puts
-end
+require_relative 'app'
 
-def create_a_teacher
-  print 'Age : '
-  age = gets.chomp
-  print 'Name : '
-  name = gets.chomp
-  print 'Specialization : '
-  specialization = gets.chomp
-  puts 'Person created successfully '
-  puts
-end
-
-def create_a_person
-  loop do
-    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number] :'
-    option = gets.chomp
-    case option
-    when '1'
-      create_a_student
-      break
-    when '2'
-      create_a_teacher
-      break
-    else
-      puts 'Option unknown'
-    end
+class Main
+  def initialize
+    @app = App.new
   end
-end
 
-def create_a_book
-  print 'Title : '
-  title = gets.chomp
-  print 'Auhor : '
-  author = gets.chomp
-  puts 'Book created successfully '
-  puts
-end
+  def create_a_student
+    print 'Age : '
+    age = gets.chomp
+    print 'Name : '
+    name = gets.chomp
+    print 'Has parent permission ? [Y/N] '
+    permission = gets.chomp
+    puts 'Person created successfully '
+    puts
+  end
 
-def create_a_rental
-  puts 'Select a book from the following list by number'
-  book = gets.chomp
-  puts 'Select a person from the following list by number'
-  person = gets.chomp
-  print 'Date : '
-  date = gets.chomp
-  puts 'Rental created successfully '
-  puts
-end
+  def create_a_teacher
+    print 'Age : '
+    age = gets.chomp
+    print 'Name : '
+    name = gets.chomp
+    print 'Specialization : '
+    specialization = gets.chomp
+    puts 'Person created successfully '
+    puts
+  end
 
-def list_rental_person
-  print 'ID of person : '
-  id = gets.chomp
-  puts 'Rentals : '
-  puts
-end
-
-def menu
-  puts 'Please choose an option by enterin a number :'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-end
-
-def main
-  puts 'Welcome to School Library App!'
-  puts
-
-  option = 0
-
-  loop do
-    menu
-    option = gets.chomp
-
-    case option
-    when '3'
-      create_a_person
-    when '4'
-      create_a_book
-    when '5'
-      create_a_rental
-    when '6'
-      list_rental_person
-    when '7'
-      break
-    else
-      puts 'Option unknown'
+  def create_a_person
+    loop do
+      puts 'Do you want to create a student (1) or a teacher (2)? [Input the number] :'
+      option = gets.chomp
+      case option
+      when '1'
+        create_a_student
+        break
+      when '2'
+        create_a_teacher
+        break
+      else
+        puts 'Option unknown'
+      end
     end
   end
 
-  puts 'Thank you for using this app!'
+  def create_a_book
+    print 'Title : '
+    title = gets.chomp
+    print 'Auhor : '
+    author = gets.chomp
+    @app.create_a_book(title, author)
+    puts 'Book created successfully '
+    puts
+  end
+
+  def create_a_rental
+    puts 'Select a book from the following list by number'
+    book = gets.chomp
+    puts 'Select a person from the following list by number'
+    person = gets.chomp
+    print 'Date : '
+    date = gets.chomp
+    puts 'Rental created successfully '
+    puts
+  end
+
+  def list_rental_person
+    print 'ID of person : '
+    id = gets.chomp
+    puts 'Rentals : '
+    puts
+  end
+
+  def menu
+    puts 'Please choose an option by enterin a number :'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
+  def list_all_books
+    @app.list_all_books.each do |b|
+      puts "Title: \"#{b.title}\", Author: #{b.author}"
+    end
+  end
+
+  def main
+    puts 'Welcome to School Library App!'
+    puts
+
+    option = 0
+
+    loop do
+      menu
+      option = gets.chomp
+
+      case option
+      when '1'
+        list_all_books
+      when '3'
+        create_a_person
+      when '4'
+        create_a_book
+      when '5'
+        create_a_rental
+      when '6'
+        list_rental_person
+      when '7'
+        break
+      else
+        puts 'Option unknown'
+      end
+    end
+
+    puts 'Thank you for using this app!'
+  end
 end
 
-main
+Main.new.main
